10564bd2caaaf8fcad36a81a1737c78050ef5666
Dell <Dell@DESKTOP-FM8464J>
2024-01-08 08:29:40 +0545

    Add project files.

---
+++ LINKQ/Program.cs
@@ -0,0 +1,19 @@
+﻿// program to create LINQ query using Query Syntax
+class HelloWorld
+{
+    static void Main()
+    {
+        int[] scroes= new int[] { 97, 92, 81, 60 };
+        //define the query expression
+        IEnumerable<int> scoreQuery =
+            //query syntax
+            from score in scroes
+            where score > 80
+            select score;
+        //execute the query
+        foreach (int i in scoreQuery)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
